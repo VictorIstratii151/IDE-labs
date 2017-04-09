99393413c6f42883044fdf9c0f9059b9a08b70cc
VictorIstratii151 <victor.istratii@faf.utm.md>
2017-04-09 19:00:36 +0300

    Added functions for basic arithmetic operations

--- lab2/scientific_calculator/calculatorLogic.cs
+++ lab2/scientific_calculator/calculatorLogic.cs
@@ -12,5 +12,25 @@ namespace calculatorLogic
         {
             return d1 + d2;
         }
+
+        public static double subtractDoubles(double d1, double d2)
+        {
+            return d1 - d2;
+        }
+
+        public static double multiplyDoubles(double d1, double d2)
+        {
+            return d1 * d2;
+        }
+
+        public static double divideDoubles(double d1, double d2)
+        {
+            return d1 / d2;
+        }
+
+        public static double modDoubles(double d1, double d2)
+        {
+            return d1 % d2;
+        }
     }
 }
